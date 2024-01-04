ec4f8c9d0e550c3f6a3e1e08db911de578536749
Gustavo Jung <80927562+gustavojung23@users.noreply.github.com>
2024-01-04 10:38:05 -0300

    Update filter.js

--- javascript-filter/script.js
+++ javascript-filter/script.js
@@ -1,1 +1,30 @@
 //filter.
+const array = [1, 2, 3, 4, 5];
+
+console.log(array);
+
+//filtrar os números maiores que 3.
+const highNumber = array.filter((numberHigh) => {
+    if(numberHigh >= 3){
+        return numberHigh;
+    }
+});
+
+console.log(highNumber);
+
+
+const users = [
+    {name: "Gustavo", available: true},
+    {name: "João", available: false},
+    {name: "Dayane", available: true},
+    {name: "Matheus", available: false},
+];
+
+//usuários True
+const availableUsers = users.filter((user) => user.available);
+
+//usuários False
+const notAvailableUsers = users.filter((user) => !user.available);
+
+console.log(availableUsers);
+console.log(notAvailableUsers);
