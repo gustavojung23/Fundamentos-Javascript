145bdb218c5eb5b78849337bf8c4f458c2c099c1
Gustavo Jung <80927562+gustavojung23@users.noreply.github.com>
2024-01-04 10:24:03 -0300

    Javascript var, let e const index and js

---
+++ javascript-var-let-const/script.js
@@ -0,0 +1,42 @@
+//var let e const.
+var x = 10;
+var y = 15;
+
+if(y > 10){
+    var x = 5
+    console.log(x);
+}
+
+console.log(x)
+
+
+let a = 10;
+let b = 15;
+
+if(b > 10){
+    let a = 5;
+    console.log(a);
+}
+
+console.log(a);
+
+
+let i = 100;
+
+for(let i = 0; i < 5; i++){
+    console.log(i);
+}
+
+console.log(i);
+
+
+function logName(){
+    const name = "Gustavo";
+    console.log(name);
+}
+
+logName();
+
+const name = "Jung";
+
+console.log(name);
